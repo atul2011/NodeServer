c9e060ce8bd6e9a8cdbf7a15a7364e07eaa03a9c
atul <atul.webdp@gmail.com>
2023-05-15 22:05:43 +0530

    node.js initialize

---
+++ config/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const helmet = require("helmet"),
+  server = express();
+const { setRoutes } = require("./routes");
+// For security
+
+server.use(helmet());
+
+const cors = require("cors"),
+  // Allow Origins according to your need.
+  corsOptions = {
+    origin: "*",
+  };
+
+server.use(cors(corsOptions));
+
+server.use(bodyParser.json());
+server.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// Setting up Routes
+setRoutes(server);
+
+module.exports = { server };
